1c940b20488b5a1f409b111640c36d1c4295c64e
KeKoaM <kai.moritamcvey@gmail.com>
2020-02-10 12:26:00 -0800

    set up twilio message

--- the_webscraper.py
+++ the_webscraper.py
@@ -1,1 +1,15 @@
+import twilio
+from twilio.rest import Client
 
+
+def output(change_message, user_config):
+    twilio_sid = user_config["twilio_sid"]
+    twilio_auth = user_config["twilio_auth"]
+    twilio_phone = user_config["twilio_phone"]
+    user_phone = user_config["user_phone"]
+    # print(twilio_sid, twilio_auth, twilio_phone, user_phone, message)
+    client = Client(twilio_sid, twilio_auth)
+    message = client.messages.create(
+        body=change_message, from_=twilio_phone, to=user_phone
+    )
+    print(message.sid)

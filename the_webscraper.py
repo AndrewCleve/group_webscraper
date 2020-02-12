310ba1f52dbce9574cae13f31f6f4614c885195c
AceBurton <acehburton@gmail.com>
2020-02-12 13:17:11 -0800

    added magic happens

--- the_webscraper.py
+++ the_webscraper.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def magic_happens():
+    url = "https://JasonCulley.github.io"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,"html.parser")
+    results = soup.find("h1")
+    print(results.text)
+if __name__ == "__main__":
+    magic_happens()
 

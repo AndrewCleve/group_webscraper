b0f3ae6fe6e061a379ab92619a22948815ed15d5
JasonCulley <jasonrculley@gmail.com>
2020-02-14 12:05:42 -0800

    esn't work, slightly better GUI

--- the_webscraper.py
+++ the_webscraper.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 ##class myClass():
-	# # def method1(self):
+	# # def method1():
 		# # print("myClass method1")
-	# # def method2(self,someString):
+	# # def method2(,someString):
 		# # print("myClass method2:" + someString )
 # # class anotherClass(myClass):
-	# # def method2(self):
+	# # def method2():
 		# # print("anotherClass")
 	# # class myClass():
-		# # def method1(self):
+		# # def method1():
 			# # print("anotherClass method1")
 # # def main():
 	# # c = myClass()
@@ -20,55 +20,57 @@ import tkinter as tk
 	
 	# # c2.method1()
 	# # c2.method2()
-  
 # # if __name__ == "__main__":
+  
   # # main()
-class GUI():
-    def RunGUI(self):
-        self.window = tk.Tk()
-        
-        l1 = tk.Label(self.window, text = "Enter website name here ")
-        l2 = tk.Label(self.window, text = "Enter checkrate here ")
-        l3 = tk.Label(self.window, text = "Enter SID here ")
-        l4 = tk.Label(self.window, text = "Enter Twilio Authorization here ")
-        l5 = tk.Label(self.window, text = "Enter Twilio phone number here ")
-        l6 = tk.Label(self.window, text = "Enter user phone number here ")
-        
-        
-        
-        website = tk.Entry(self.window,  )
-        checkrate = tk.Entry(self.window )
-        sid = tk.Entry(self.window )
-        auth = tk.Entry(self.window )
-        twilphone = tk.Entry(self.window )
-        userphone = tk.Entry(self.window )
-        
-        button = tk.Button(self.window, text ="Finish", width = 25, command = self.end() )
-        
-        l1.grid(row = 0, column = 0, pady =2)
-        l2.grid(row = 1, column = 0, pady =2)
-        l3.grid(row = 2, column = 0, pady =2)
-        l4.grid(row = 3, column = 0, pady =2)
-        l5.grid(row = 4, column = 0, pady =2)
-        l6.grid(row = 5, column = 0, pady =2)
-        
-        website.grid(row = 0, column = 2, pady = 2)
-        checkrate.grid(row = 1, column = 2, pady = 2)
-        sid.grid(row = 2, column = 2, pady = 2)
-        auth.grid(row = 3, column = 2, pady = 2)
-        twilphone.grid(row = 4, column = 2, pady = 2)
-        userphone.grid(row = 5, column = 2, pady = 2)
-        
-        button.grid(row=6, column =1 )
-        
-        
-        
-        self.window.mainloop()
-    def end(self):
-        self.window.destroy
+window = tk.Tk()
+def RunGUI():
     
+    
+    l1 = tk.Label(window, text = "Enter website name here ")
+    l2 = tk.Label(window, text = "Enter checkrate here ")
+    l3 = tk.Label(window, text = "Enter SID here ")
+    l4 = tk.Label(window, text = "Enter Twilio Authorization here ")
+    l5 = tk.Label(window, text = "Enter Twilio phone number here ")
+    l6 = tk.Label(window, text = "Enter user phone number here ")
+    
+    
+    
+    website = tk.Entry(window,  )
+    checkrate = tk.Entry(window )
+    sid = tk.Entry(window )
+    auth = tk.Entry(window )
+    twilphone = tk.Entry(window )
+    userphone = tk.Entry(window )
+    website.set("test")
+    a = (website.get())
+    print(a)
+    button = tk.Button(window, text ="Finish", width = 25, command = end )
    
+    
+    l1.grid(row = 0, column = 0, pady =2)
+    l2.grid(row = 1, column = 0, pady =2)
+    l3.grid(row = 2, column = 0, pady =2)
+    l4.grid(row = 3, column = 0, pady =2)
+    l5.grid(row = 4, column = 0, pady =2)
+    l6.grid(row = 5, column = 0, pady =2)
+    
+    website.grid(row = 0, column = 2, pady = 2)
+    checkrate.grid(row = 1, column = 2, pady = 2)
+    sid.grid(row = 2, column = 2, pady = 2)
+    auth.grid(row = 3, column = 2, pady = 2)
+    twilphone.grid(row = 4, column = 2, pady = 2)
+    userphone.grid(row = 5, column = 2, pady = 2)
+    
+    button.grid(row=6, column =1 )
+    
+    
+    window.mainloop()
+def end():
+    print("hello world")
+    window.destroy()
+
+
 if __name__ == "__main__":
-    NewGUI = GUI()
-    NewGUI.RunGUI()
+    RunGUI()
     

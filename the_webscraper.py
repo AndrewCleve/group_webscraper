3c323294f9582ebd7b03aed1bdbe49f2d0f291a8
AndrewCleve <45438487+AndrewCleve@users.noreply.github.com>
2020-02-25 13:12:00 -0800

    Update the_webscraper.py

--- the_webscraper.py
+++ the_webscraper.py
@@ -1,23 +1,23 @@
 import twilioauth as twauth
 
 def main():
-    tempsid = input("Enter your session ID : ")
-    if tempsid != '':
-        twauth.twiliodict['sid'] = tempsid
-    
-    tempauth = input("Enter your Twilio Auth : ")
-    if tempauth != '':
-        twauth.twiliodict['auth'] = tempauth
-    
-    temptwilphone = input("Enter your Twilio phone # : ")
-    if temptwilphone != '':
-        twauth.twiliodict['twilphone'] = temptwilphone
-    
-    tempuserphone = input("Enter your phone # to send to : ")
-    if tempuserphone != '':
-        twauth.twiliodict['userphone'] = tempuserphone
-    for key in twauth.twiliodict:
-        print(key, ",", twauth.twiliodict[key])
+#    tempsid = input("Enter your session ID : ")
+#    if tempsid != '':
+#        twauth.twiliodict['sid'] = tempsid
+#    
+#    tempauth = input("Enter your Twilio Auth : ")
+#    if tempauth != '':
+#        twauth.twiliodict['auth'] = tempauth
+#    
+#    temptwilphone = input("Enter your Twilio phone # : ")
+#    if temptwilphone != '':
+#        twauth.twiliodict['twilphone'] = temptwilphone
+#    
+#    tempuserphone = input("Enter your phone # to send to : ")
+#    if tempuserphone != '':
+#        twauth.twiliodict['userphone'] = tempuserphone
+#    for key in twauth.twiliodict:
+#        print(key, ",", twauth.twiliodict[key])
     
 if __name__ == "__main__":
     main()
